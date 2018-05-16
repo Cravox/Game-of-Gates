fe42253ad5203a0ca542e34ece047f7ad1b4869e
Unknown <janik.hofmann@games-academy.de>
2018-05-16 14:15:20 +0200

    init

---
+++ Assets/Script/SpreadBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadBullet : MonoBehaviour
+{
+
+    void Start()
+    {
+        Destroy(this.gameObject, 0.3f);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Enemy")) col.gameObject.GetComponent<Enemy>().hp -= 2;
+    }
+}
